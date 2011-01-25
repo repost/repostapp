2d2466afca35692160cda8994d09af67fe739670
hank <hank@bc4a479e-3a1a-4069-b84a-023495cab333>
2011-01-25 05:50:18 +0000

    updated the content script

--- content.js
+++ content.js
@@ -5,12 +5,26 @@ chrome.extension.sendRequest({greeting: "hello"}, function(response) {
 
 
 contentClicker = function() {
+
+    var ImgDialog
     this.init = function(){
+       ImgDialog = document.createElement('div');
+        ImgDialog.setAttribute('class', 'imgdialog');
+        ImgDialog.style.visibility = 'hidden';
+        document.body.appendChild(ImgDialog);
+
         document.addEventListener('click',this.clickListener,false);
     };
 
     this.clickListener = function(event){
         console.log(event.target);
+        cc.createImgDialog("hello");
+    };
+
+    this.createImgDialog = function(image){
+        caption = document.createElement("input");
+        ImgDialog.appendChild(caption);
+        ImgDialog.style.visibility = 'visible';
     };
 };
 
