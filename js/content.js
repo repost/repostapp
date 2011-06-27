2e375e1771499b44812beda0e445e7e9088450d4
hank <hank@bc4a479e-3a1a-4069-b84a-023495cab333>
2011-06-27 05:57:00 +0000

    Some more changes were missed during the merge

--- js/content.js
+++ js/content.js
@@ -1,83 +1,142 @@
-contentClicker = function() {
+this.imagePostBox = function(sendPostCB){
 
-    var ImgDialog;
+    // Dialog
+    var imagePostBox;
+
+    // Content
+    var image;
     var caption;
-    var cur_post;
+    var curpost; /* captured over two events. Cleanest way */
+
+    // Send Post callback
+    var spCB = sendPostCB;
 
     this.init = function(){
-        // Create image dialog
-        RepostDialog = document.createElement('div');
-        RepostDialog.className = "repostdialog";
-        var label = document.createElement("label");
+
+        var close; /* close button */
+        var label; /* temp label */
+        var postbutton; /* send that text */
+
+        // Create Dialog
+        imagePostBox = document.createElement('div');
+        imagePostBox.className = "repostdialog";
+        // Caption
+        label = document.createElement("label");
         label.innerHTML = "What say you:";
         label.className = "label";
-        RepostDialog.appendChild(label);
         caption = document.createElement("input");
         caption.className = "caption";
+        caption.addEventListener('keypress', this.submitPost(), false);
         label.appendChild(caption);
-        caption.addEventListener('keypress', this.sendContent, false);
-        RepostDialog.style.visibility = "hidden";
-        document.body.appendChild(RepostDialog);
-        
+        imagePostBox.appendChild(label);
+        imagePostBox.style.visibility = "hidden";
+        // 'X'
+        close = document.createElement("span");
+        close.innerHTML = "x";
+        close.className = "floatclose";
+        close.onclick = this.onclickclose(this);
+        imagePostBox.appendChild(close);
+        // Append
+        document.body.appendChild(imagePostBox);
+
         // add event listener to each image on the page
         var images = document.getElementsByTagName("img");
         for( var i = 0; i < images.length; i++ ){
-            images[i].addEventListener('click',this.imgClickListener,false);
+            images[i].addEventListener('click',this.imgClickListener(this),false);
         }
+
     };
 
-    this.imgFocus = function(){
+    this.onclickclose = function(popup){
+        return function(){
+            popup.close();
+        };
+    };
+
+    this.close = function(){
+        this.clear();
+        imagePostBox.style.visibility = "hidden";
+    };
+
+    this.clear = function(){
+        caption.value = "";
+        image.value = "";
+    };
+
+    this.focus = function(){
         caption.focus();
     };
-    
-    this.imgClear = function(){
-        caption.value = "";
+
+    this.submitPost = function(postbox){
+        return function(event){
+            if(event.keyCode == 13){
+                curpost.setCaption(caption.value);
+                spCB(curpost);
+                imagePostBox.style.visibility = "hidden";
+                postbox.clear();
+            }
+        };
     };
 
-    this.sendContent = function(event){
-        if(event.keyCode == 13){
-            cur_post.setCaption(caption.value);
-            chrome.extension.sendRequest(JSON.stringify(cur_post), 
-                function(response) {
+    this.imgClickListener = function(postbox){
+        return function(event){
+            if(event.altKey){
+                curpost = new postImage();
+                curpost.setImage(event.currentTarget.src);
+                curpost.setContext(event.currentTarget.baseURI);
+                postbox.display(event.clientX, event.clientY);
+                postbox.focus();
+                event.returnValue = false;
+                return false;
+            }
+        };
+    };
+
+    this.display = function(x, y){
+        imagePostBox.style.visibility = "visible";
+        imagePostBox.style.top = y + "px";
+        imagePostBox.style.left = x + "px";
+    };
+
+    this.init();
+
+}
+
+this.contentClicker = function() {
+
+    var imagebox;
+    var textbox;
+
+    this.init = function(){
+        imagebox = new imagePostBox(this.sendPost());
+        //TODO once we have fixed rss we can uncomment this and text post anywhere
+        //textbox = new textPostBox(this.sendPost());
+        document.addEventListener("keydown", this.shortcuts());
+    };
+
+    this.shortcuts = function(contentclicker){
+        return function(e){
+            if(e.altKey){
+                var code = e.keyCode;
+                var c = String.fromCharCode(code).toLowerCase();
+                if(c == "t"){ // Text Post Box Popup
+                    //TODO once we have fixed rss we can uncomment this and text post anywhere
+                    //textbox.display();
+                }
+            }
+        };
+    };
+
+    this.sendPost = function(){
+        return function(post){
+            chrome.extension.sendRequest(JSON.stringify(post), 
+                    function(response) {
                     console.log(response.farewell);
-                });
-            RepostDialog.style.visibility = "hidden";
-            cc.imgClear();
-        }
+                    });
+        };
     };
-
-    this.imgClickListener = function(event){
-        if(event.altKey){
-            cur_post = new postImage();
-            cur_post.setImage(event.currentTarget.src);
-            cur_post.setContext(event.currentTarget.baseURI);
-            cc.createRepostDialog(event.currentTarget.src,
-                        event.clientX, event.clientY);
-            cc.imgFocus();
-            event.returnValue = false;
-            return false;
-        }
-    };
-
-    this.linkClickListner = function(event){
-        if(event.altKey){
-            cur_post = new postText();
-            cur_post.setLink(event.currentTarget.baseURI);
-            cc.createRepostDialog(event.currentTarget.src,
-                            event.clientX, event.clientY);
-            cc.imgFocus();
-            event.returnValue = false;
-            return false;
-        }
-    };
-
-    this.createRepostDialog = function(image, x, y){
-        RepostDialog.style.visibility = "visible";
-        RepostDialog.style.top = y + "px";
-        RepostDialog.style.left = x + "px";
-    };
+    this.init();
 };
 
 var cc = new contentClicker();
-cc.init();
 
