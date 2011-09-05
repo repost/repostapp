f3176d7bab9b7c0751ba15b1734413f517c84032
hankhank <andrew@sideramota.com>
2011-09-05 21:27:48 +1000

    Few style changes to text positions

--- js/postimage.js
+++ js/postimage.js
@@ -125,8 +125,8 @@ $.fn.textWidth = function(){
                     .mouseover($.proxy(post.mover, post))
                     .mouseout($.proxy(post.mout, post));
             var mouseover = $('<div>').addClass('postmask')
-                                .append($('<div>'+this.opts.masktext+'</div>')
-                                        .addClass('postmaskcaption'))
+                                .append($('<div>').append($('<div>'+this.opts.masktext+'</div>')
+                                        .addClass('postmaskcaption')))
                                 .append($('<image>')
                                         .attr('src','/images/repost_r.gif')
                                         .addClass('upvote')
