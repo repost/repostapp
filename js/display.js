d0e68aa9c1b0a4ed210bc9129c21f59f7212c707
harry <harry@serenity.(none)>
2011-09-05 15:22:01 +1000

    initial help screen

--- js/display.js
+++ js/display.js
@@ -61,72 +61,75 @@ function checkForPost(post,rank) {
 };
 
 this.repostNotification = function(){
+    var msgQueue;
+    var timeout;
 
-  var msgQueue;
-  var timeout;
-
-  this.init = function(){
-    this.msgQueue = new Array();
-  };
-  
-  this.sendMsg = function(msg){
-    // Create a simple text notification:
-    var notification = webkitNotifications.createNotification(
-        'images/icon-16.jpeg',  // icon url - can be relative
-        'New Repost:',
-        msg
-        );
-    notification.onclick = function(){ window.focus(); this.cancel(); };
-    setTimeout(function(){ notification.cancel();}, '5000');
-    notification.show();
-  };
-  
-  this.notification = function(title, msg){
-    // Create a simple text notification:
-    var notification = webkitNotifications.createNotification(
-        'images/icon-16.jpeg',  // icon url - can be relative
-        title,
-        msg
-        );
-    notification.onclick = function(){ window.focus(); this.cancel(); };
-    setTimeout(function(){ notification.cancel();}, '5000');
-    notification.show();
-  };
-
-  // On the timeout if there is only 1 post send out the caption
-  // more than one send out the number
-  this.onTimeOut = function(){
-    this.timeout = null;
-    if(this.msgQueue.length == 1){
-      this.sendMsg(this.msgQueue.pop());
-    }else if( this.msgQueue.length > 1){
-      this.sendMsg(this.msgQueue.length + " New Posts");
-      while(this.msgQueue.length > 0){
-        this.msgQueue.pop();
-      }
-    }
-  };
-  
-  // Need to make timeout a closure so we can access our object
-  this.closedOnTimeOut = function(){
-    var _this = this;
-    return function(){
-      _this.onTimeOut();
+    this.init = function(){
+        this.msgQueue = new Array();
     };
-  };
 
-  // Queue up notifications so we don't get an explosition across the screen
-  this.queueNotification = function(message){
-    if(message){
-      this.msgQueue.push(message);
-      if(this.timeout == null){
-        this.timeout = setTimeout(this.closedOnTimeOut(), '1000'); // Time to queue em up before flushing
-      }
-    }
-  };
+    this.sendMsg = function(msg){
+        // Create a simple text notification:
+        var notification = webkitNotifications.createNotification(
+                'images/icon-16.jpeg',  // icon url - can be relative
+                'New Repost:',
+                msg
+                );
+        notification.onclick = function(){ window.focus(); this.cancel(); };
+        setTimeout(function(){ notification.cancel();}, '5000');
+        notification.show();
+    };
 
-  this.init();
+    this.notification = function(title, msg){
+        // Create a simple text notification:
+        var notification = webkitNotifications.createNotification(
+                'images/icon-16.jpeg',  // icon url - can be relative
+                title,
+                msg
+                );
+        notification.onclick = function(){ window.focus(); this.cancel(); };
+        setTimeout(function(){ notification.cancel();}, '5000');
+        notification.show();
+    };
 
+    // On the timeout if there is only 1 post send out the caption
+    // more than one send out the number
+    this.onTimeOut = function(){
+        this.timeout = null;
+        if(this.msgQueue.length == 1){
+            this.sendMsg(this.msgQueue.pop());
+        }else if( this.msgQueue.length > 1){
+            this.sendMsg(this.msgQueue.length + " New Posts");
+            while(this.msgQueue.length > 0){
+                this.msgQueue.pop();
+            }
+        }
+    };
+
+    // Need to make timeout a closure so we can access our object
+    this.closedOnTimeOut = function(){
+        var _this = this;
+        return function(){
+            _this.onTimeOut();
+        };
+    };
+
+    // Queue up notifications so we don't get an explosition across the screen
+    this.queueNotification = function(message){
+        if(message){
+            this.msgQueue.push(message);
+            if(this.timeout == null){
+                this.timeout = setTimeout(this.closedOnTimeOut(), '1000'); // Time to queue em up before flushing
+            }
+        }
+    };
+
+    this.init();
+};
+
+// help stuff
+this.helpVisual = function() {
+    alert("hello world");
 };
 
 
@@ -159,6 +162,9 @@ function addShortCuts(){
     $('#connlink').click(function(){ // Text Post Box Popup
                 linksdisplay.show();       
             });
+    $('#helplink').click(function(){
+        helpdisplay.show();
+    }
 };
 
 function checkStatus() {
@@ -181,8 +187,11 @@ var repostNotify;
 var wel;
 var statusBar;
 
+var help;
+
 function main() {
     $('document').ready(function(){
+        help = new helpVisual();
         linksdisplay = new linkVisual();
         // Create instance of plugin
         plugin = document.getElementById("plugin");
